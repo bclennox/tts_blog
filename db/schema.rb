5482b7a23c507915672e65614c2f7744c85d737e
Brandan Lennox <brandan@bclennox.com>
2014-11-05 22:51:10 -0500

    Adding the Modern Business theme.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022012103) do
+ActiveRecord::Schema.define(version: 20141023123603) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
     t.text     "body"
+    t.string   "author"
   end
 
 end
